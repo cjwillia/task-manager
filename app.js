023dc219634fd58214578745ebb99df89bb17186
Cory Williams <cjwillia@andrew.cmu.edu>
2015-09-12 23:23:23 -0400

    work on hooking up mongodb and mongoose to the app

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 var express = require("express");
 var app = express();
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error: '));
+
+db.once('open', function(callback) {
+	console.log('ayy, database is hooked up');
+});
+
+var UserSchema = mongoose.Schema({
+	username: {
+		type: String,
+		required: true,
+		index: {
+			unique: true
+		}
+	},
+	password: {
+		type: String,
+		required: true
+	}
+});
+
+var User = mongoose.model('User', UserSchema);
 
 app.use(express.static('client'));
 app.use('/bower_components', express.static('bower_components'));
